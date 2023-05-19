90f9e784b35b46952cca7626fb1a77220bae7d4a
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-19 20:50:48 +0800

    added comments file, and renamed

---
+++ src/partials/comments.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Comments = (props: Props) => {
+  return (
+    <View>
+      <Text>comments</Text>
+    </View>
+  )
+}
+
+export default Comments
+
+const styles = StyleSheet.create({})
