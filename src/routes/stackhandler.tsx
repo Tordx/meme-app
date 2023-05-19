1a98f94a7554ef670bdc88b441093919f3dc3997
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-19 20:50:55 +0800

    added comments

--- src/routes/stackhandler.tsx
+++ src/routes/stackhandler.tsx
@@ -5,9 +5,11 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Login from '../pages/login';
 import { Dark, light, white } from '../Assets/Colors';
 import Toptabhandler from './toptabhandler';
+import Comments from '../partials/comments';
 
 type Props = {
 
+    name?: (value: string) => void;
 
 }
 export const StackHandler: FC<Props> = (props: Props) => {
@@ -17,7 +19,7 @@ export const StackHandler: FC<Props> = (props: Props) => {
 
     return (
     <NavigationContainer>
-         <StatusBar 
+         <StatusBar
          backgroundColor={colorScheme? Dark : light} barStyle = {colorScheme ? 'light-content': 'dark-content'} 
          />
         <Stack.Navigator screenOptions={{gestureVelocityImpact: 0.1,}}>
@@ -34,6 +36,13 @@ export const StackHandler: FC<Props> = (props: Props) => {
                 options={{
                     headerShown: false
                 }}
+            />
+            <Stack.Screen
+             name = 'comments' 
+             component={Comments} 
+             options={{
+                 headerShown: false
+             }}
             />
         </Stack.Navigator>
     </NavigationContainer>
