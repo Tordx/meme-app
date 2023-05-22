31670324a7898bbed106a533807ddfd602d7d95d
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-22 23:56:45 +0800

    api

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,4 +2,7 @@ declare module '@env' {
     export const MEMEDB: string;
     export const MEMEUPVOTE: string;
     export const MEMEACCOUNT: string;
+    export const MEMECOMMENT: string,
+    export const MEMEVIDEO: string,
+    export const PEXELAPI: string,
   }
