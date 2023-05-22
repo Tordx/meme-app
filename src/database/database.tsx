8831fdd48a0e3e8a26d2bbcdab99cd5cbe3fd6e7
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-22 23:56:51 +0800

    db for vid added

--- src/database/database.tsx
+++ src/database/database.tsx
@@ -2,3 +2,5 @@ import PouchDB from 'pouchdb-react-native' ; 'pouchdb-core';
 export const dbMeme  = new PouchDB(process.env.MEMEDB);
 export const dbMemevote = new PouchDB(process.env.MEMEUPVOTE);
 export const dbMemeaccount = new PouchDB(process.env.MEMEACCOUNT)
+export const dbMemecomment =  new PouchDB(process.env.MEMECOMMENT)
+export const dbMemevideo = new PouchDB(process.env.MEMEVIDEO)
