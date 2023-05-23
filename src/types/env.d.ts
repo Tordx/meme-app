b01de0b0a0a575882584183ec969715dd523a616
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-23 22:05:54 +0800

    added env variables

--- src/types/env.d.ts
+++ src/types/env.d.ts
@@ -2,7 +2,8 @@ declare module '@env' {
     export const MEMEDB: string;
     export const MEMEUPVOTE: string;
     export const MEMEACCOUNT: string;
-    export const MEMECOMMENT: string,
-    export const MEMEVIDEO: string,
-    export const PEXELAPI: string,
+    export const MEMECOMMENT: string;
+    export const MEMEVIDEO: string;
+    export const PEXELAPI: string;
+    export const IMGURAPI: string;
   }
