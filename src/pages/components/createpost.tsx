d4c516889d48e8701d0c261eccea0004722b4317
Roven Kyle Narra <ellamateogang@gmail.com>
2023-05-23 22:04:15 +0800

    video db changed to imgur

--- src/pages/components/createpost.tsx
+++ src/pages/components/createpost.tsx
@@ -6,14 +6,34 @@ import RNFetchBlob from 'rn-fetch-blob'
 import RNFS from 'react-native-fs';
 import { generateId } from '../../library/idgeneration';
 import { dbMemevideo } from '../../database/database';
+import { useSelector } from 'react-redux'
 
 type Props = {}
 
+interface userdata {
+  user: {
+    useraccount: {
+      _id: string;
+      _rev: string;
+      username: string;
+      userimage: string;
+      firstname: string;
+      lastname: string;
+      password: string;
+      userid: string;
+      status: string;
+      usertype: string;
+    }
+  }
+}
 
 const Createpost = (props: Props) => {
 
     const colorScheme = useColorScheme() === 'dark';
+    const {useraccount} =  useSelector((action: userdata) => action.user)
     const [video, setvideo] = useState('');
+    const date = new Date()
+    const timestamp = date.getDate();
 
     const uploadVideo = async () => {
         const option = {
@@ -29,21 +49,41 @@ const Createpost = (props: Props) => {
             console.log('User cancelled video picker');
           } else if (response) {
             const file = response;
+            console.log(file);
+            
             try {
+              
               const data = file?.assets?.[0]?.uri;
               console.log(data);
-              const videoBlob = await RNFetchBlob.fs.readFile(data as string, 'base64');
-              const doc = {
-                _id: id,
-                _attachments: {
-                  video: {
-                    content_type: data,
-                    data: videoBlob,
-                  },
+              const formData = new FormData();
+              formData.append('video', {
+                uri: data,
+                type: 'video/mp4',
+                name: 'video.mp4',
+              });
+              console.log(formData);
+              const response = await fetch('https://api.imgur.com/3/upload', {
+                method: 'POST',
+                headers: {
+                  'Authorization': 'Bearer bd49a5b019e13ffe584a4c735069141287166b0c',
+                  'Content-Type': 'multipart/form-data',
                 },
-              };
-              console.log(videoBlob)
-              await dbMemevideo.put(doc);
+                body: formData,
+              });
+              const result = await response.json();
+              console.log(result);
+              const videolink = result.data.link
+              const id = generateId()
+              const newvideo = {
+                _id: id,
+                video: videolink,
+                username: useraccount.username,
+                userimage: useraccount.userimage,
+                timestamp: timestamp,
+              }
+              await dbMemevideo.put(newvideo)
+              console.log(newvideo);
+              
             } catch (error) {
               console.log('Error uploading video:', error);
             }
